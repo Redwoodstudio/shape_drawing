7c24a81fd56942b34d9146a4df586e88e2de0105
Redwoodstudio <redwoodstudio@seznam.cz>
2022-05-14 14:13:30 +0200

    Changed shape rendering to use the bevy_prototype_lyon crate

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use bevy::input::mouse::{MouseMotion, MouseWheel};
 use bevy::prelude::*;
+use bevy_prototype_lyon::prelude::*;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugin(ShapePlugin)
         .add_startup_system(spawn_camera)
         .add_system(mouse_position)
         .add_system(spawn_rectangle)
@@ -31,20 +33,21 @@ fn spawn_rectangle(
     mut commands: Commands,
     mut windows: ResMut<Windows>,
     mouse_input: Res<Input<MouseButton>>,
-    mut query: Query<(&mut Sprite, Entity), With<Moving>>,
+    mut query: Query<(&mut DrawMode, Entity), With<Moving>>,
     mouse: Res<MouseMovement>,
 ) {
     let window = windows.get_primary_mut().unwrap();
     if mouse_input.just_pressed(MouseButton::Left) {
+        let rect = shapes::Rectangle {
+            extents: Vec2::ZERO,
+            ..default()
+        };
         commands
-            .spawn_bundle(SpriteBundle {
-                sprite: Sprite {
-                    custom_size: Some(Vec2::new(1.0, 1.0)),
-                    ..default()
-                },
-                transform: Transform::from_translation(mouse.position.extend(0.0)),
-                ..default()
-            })
+            .spawn_bundle(GeometryBuilder::build_as(
+                &rect,
+                DrawMode::Fill(FillMode::color(Color::WHITE)),
+                Transform::from_translation(mouse.position.extend(0.0)),
+            ))
             .insert(Moving {
                 origin: mouse.position,
             });
@@ -52,30 +55,22 @@ fn spawn_rectangle(
     }
 
     if mouse_input.just_released(MouseButton::Left) {
-        let (mut rectangle_sprite, id) = query.get_single_mut().unwrap();
-        rectangle_sprite.color = Color::Rgba {
-            red: 1.0,
-            green: 0.0,
-            blue: 0.0,
-            alpha: 1.0,
-        };
+        let (mut rectangle_draw_mode, id) = query.get_single_mut().unwrap();
+        if let DrawMode::Fill(ref mut fill_mode) = *rectangle_draw_mode {
+            fill_mode.color = Color::RED;
+        }
         commands.entity(id).remove::<Moving>();
         window.set_cursor_lock_mode(false);
     }
 }
 
-fn mouse_motion(
-    mouse: Res<MouseMovement>,
-    mut query: Query<(&mut Sprite, &mut Transform, &Moving)>,
-) {
-    if let Ok((mut item_sprite, mut item_transform, moving)) = query.get_single_mut() {
-        let size = mouse.position - moving.origin;
-        item_sprite.custom_size = Some(size);
-        item_transform.translation = Vec3::new(
-            moving.origin.x + size.x / 2.0,
-            moving.origin.y + size.y / 2.0,
-            0.0,
-        );
+fn mouse_motion(mouse: Res<MouseMovement>, mut query: Query<(&mut Path, &Moving)>) {
+    if let Ok((mut path, moving)) = query.get_single_mut() {
+        let rect = shapes::Rectangle {
+            extents: (mouse.position - moving.origin) * Vec2::new(1.0, -1.0),
+            origin: RectangleOrigin::TopLeft,
+        };
+        *path = ShapePath::build_as(&rect);
     }
 }
 
@@ -104,18 +99,22 @@ fn camera_zoom(
 ) {
     let delta_zoom: f32 = whl.iter().map(|e| e.y).sum();
     let mut delta_movement = Vec2::ZERO;
-    for i in mouse_movement.iter().map(|e| Vec2::new(e.delta.x, -e.delta.y)) {
-        delta_movement-=i;
+    for i in mouse_movement
+        .iter()
+        .map(|e| Vec2::new(e.delta.x, -e.delta.y))
+    {
+        delta_movement -= i;
     }
-    if (delta_movement != Vec2::ZERO && mouse_button.pressed(MouseButton::Middle)) || delta_zoom != 0. {
+    if (delta_movement != Vec2::ZERO && mouse_button.pressed(MouseButton::Middle))
+        || delta_zoom != 0.
+    {
         let (mut pos, mut cam) = cam.single_mut();
         cam.scale -= 0.5 * delta_zoom * cam.scale;
         cam.scale = cam.scale.clamp(0.1, 1000.0);
 
         pos.translation = (mouse.position
-            - mouse.normalized * Vec2::new(cam.right, cam.top) * cam.scale + delta_movement*cam.scale)
+            - mouse.normalized * Vec2::new(cam.right, cam.top) * cam.scale
+            + delta_movement * cam.scale)
             .extend(pos.translation.z);
-
     }
-
 }
